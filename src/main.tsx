3770bdf10194dc25ad12497b2254433298862000
zakicerdas <zakitoriq1@gmail.com>
2025-10-29 07:39:52 +0700

    perbaikan bug

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
